ececb5e19e9f6e2bc4acc8ec8fd96eea60698797
wormsparty <wormsparty@gmail.com>
2023-11-19 19:43:00 +0100

    Add folder choice button

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ static bool send_message() {
     soup_message_set_request_body_from_bytes(
         message, "application/x-www-form-urlencoded", args_b);
 
-    GError *error = NULL;
+    GError *error = nullptr;
     g_autoptr(GBytes) bytes = soup_session_send_and_read(session, message, nullptr, &error);
 
     if (error) {
@@ -21,7 +21,7 @@ static bool send_message() {
         return false;
     }
 
-    g_print ("Received: %s\n", (char*)g_bytes_get_data(bytes, NULL));
+    g_print ("Received: %s\n", (char*)g_bytes_get_data(bytes, nullptr));
     return true;
 }
 
@@ -40,18 +40,42 @@ int main (int argc, char **argv) {
       gtk_grid_set_column_spacing(grid, 5);
 
       GtkWidget *button_send = gtk_button_new_with_label("Send message");
+      GtkWidget *button_folder = gtk_button_new_with_label("Choose folder");
       GtkWidget *button_quit = gtk_button_new_with_label("Quit");
 
       g_signal_connect (G_OBJECT (button_send), "clicked",
                         G_CALLBACK (send_message), nullptr);
 
       g_signal_connect (G_OBJECT (button_quit), "clicked",
-                        G_CALLBACK (+[](GtkWidget*, gpointer app) {
-                            g_application_quit(G_APPLICATION(app));
-                        }), app);
+        G_CALLBACK (+[](GtkWidget*, gpointer app) {
+            g_application_quit(G_APPLICATION(app));
+        }), app);
+
+      g_signal_connect (G_OBJECT (button_folder), "clicked",
+        G_CALLBACK (+[](GtkWidget*, gpointer window) {
+            auto chooser = gtk_file_chooser_native_new("Choose a folder", GTK_WINDOW(window),
+                               GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                               "_Open", "_Cancel");
+
+            g_signal_connect(chooser, "response",
+                G_CALLBACK(+[](GtkFileChooser* chooser, GtkResponseType response, gpointer) {
+                    if (response == GTK_RESPONSE_ACCEPT) {
+                        g_autoptr(GFile) folderpath = gtk_file_chooser_get_current_folder(chooser);
+                        gchar* foldername = g_file_get_path(folderpath);
+                        g_print("%s\n", foldername);
+                        g_free(foldername);
+                    } else {
+                        g_print("Canceled\n");
+                    }
+                }), nullptr);
+
+            gtk_native_dialog_set_modal(GTK_NATIVE_DIALOG (chooser), true);
+            gtk_native_dialog_show(GTK_NATIVE_DIALOG (chooser));
+        }), window);
 
       gtk_grid_attach(grid, button_send, 0, 0, 1, 1);
-      gtk_grid_attach(grid, button_quit, 0, 1, 1, 1);
+      gtk_grid_attach(grid, button_folder, 0, 1, 1, 1);
+      gtk_grid_attach(grid, button_quit, 0, 2, 1, 1);
 
       gtk_window_set_child(window, GTK_WIDGET (grid));
       gtk_window_present (window);
